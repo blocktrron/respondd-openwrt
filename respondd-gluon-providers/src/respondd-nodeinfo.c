98b8fdfd3352d87d27b7445dee108e62985493f0
David Bauer <mail@david-bauer.net>
2023-03-28 13:51:40 +0200

    gluon-providers: add interface adresses

--- respondd-gluon-providers/src/respondd-nodeinfo.c
+++ respondd-gluon-providers/src/respondd-nodeinfo.c
@@ -10,12 +10,85 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <netlink/netlink.h>
+#include <netlink/msg.h>
+#include <net/if.h>
+#include <netinet/in.h>
+
+#include <linux/if_addr.h>
+#include <linux/rtnetlink.h>
+
 #define RELEASE_KEY "OPENWRT_RELEASE=\""
 
 struct respondd_nodeinfo_data {
 	struct json_object *node_id;
 	struct json_object *primary_mac;
 };
+
+struct ip_address_information {
+	unsigned int ifindex;
+	struct json_object *addresses;
+};
+
+static int get_addresses_cb(struct nl_msg *msg, void *arg) {
+	struct ip_address_information *info = (struct ip_address_information*) arg;
+
+	struct nlmsghdr *nlh = nlmsg_hdr(msg);
+	struct ifaddrmsg *msg_content = NLMSG_DATA(nlh);
+	int remaining = nlh->nlmsg_len - NLMSG_LENGTH(sizeof(struct ifaddrmsg));
+	struct rtattr *hdr;
+
+	for (hdr = IFA_RTA(msg_content); RTA_OK(hdr, remaining); hdr = RTA_NEXT(hdr, remaining)) {
+		char addr_str_buf[INET6_ADDRSTRLEN];
+
+		/* We are only interested in IP-addresses of br-client */
+		if (hdr->rta_type != IFA_ADDRESS ||
+			msg_content->ifa_index != info->ifindex ||
+			msg_content->ifa_flags & (IFA_F_TENTATIVE|IFA_F_DEPRECATED)) {
+			continue;
+		}
+
+		if (inet_ntop(AF_INET6, (struct in6_addr *) RTA_DATA(hdr), addr_str_buf, INET6_ADDRSTRLEN)) {
+			json_object_array_add(info->addresses, json_object_new_string(addr_str_buf));
+		}
+	}
+
+	return NL_OK;
+}
+
+static struct json_object *get_addresses(const char *iface) {
+	struct ip_address_information info = {
+		.ifindex = if_nametoindex(iface),
+		.addresses = json_object_new_array(),
+	};
+	int err;
+
+	/* Open socket */
+	struct nl_sock *socket = nl_socket_alloc();
+	if (!socket) {
+		return info.addresses;
+	}
+
+	err = nl_connect(socket, NETLINK_ROUTE);
+	if (err < 0) {
+		goto out_free;
+	}
+
+	/* Send message */
+	struct ifaddrmsg rt_hdr = { .ifa_family = AF_INET6, };
+	err = nl_send_simple(socket, RTM_GETADDR, NLM_F_REQUEST | NLM_F_ROOT, &rt_hdr, sizeof(struct ifaddrmsg));
+	if (err < 0) {
+		goto out_free;
+	}
+
+	/* Retrieve answer. Message is handled by get_addresses_cb */
+	nl_socket_modify_cb(socket, NL_CB_VALID, NL_CB_CUSTOM, get_addresses_cb, &info);
+	nl_recvmsgs_default(socket);
+
+out_free:
+	nl_socket_free(socket);
+	return info.addresses;
+}
 
 static struct uci_section * get_first_section(struct uci_package *p, const char *type) {
 	struct uci_element *e;
@@ -57,7 +130,7 @@ static struct json_object *get_location()
 
 	struct uci_package *p;
 	if (!uci_load(ctx, "respondd-gluon", &p)) {
-		struct uci_section *s = get_first_section(p, "nodeinfo");
+		struct uci_section *s = get_first_section(p, "location");
 		if (!s)
 			goto out;
 		
@@ -192,6 +265,7 @@ struct json_object * respondd_provider_nodeinfo(void) {
 
 	struct json_object *network = json_object_new_object();
 	json_object_object_add(network, "mac", json_object_new_string(primary_mac));
+	json_object_object_add(network, "addresses", get_addresses("eth1.30"));
 	json_object_object_add(ret, "network", network);
 
 	struct json_object *software = json_object_new_object();
